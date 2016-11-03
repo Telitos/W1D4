b63b98153178d8dcc03eb0f38fd23c5c54f37cec
Ghislain H <telitos@gmail.com>
2016-11-03 21:49:26 +0000

    adds countdownGenerator to usingClosure

--- usingClosure.js
+++ usingClosure.js
@@ -17,3 +17,29 @@ const loadedDie = (function () {
 console.log(loadedDie());  // 5
 console.log(loadedDie());  // 4
 console.log(loadedDie());  // 6
+
+
+
+
+const countdownGenerator = function (x) {
+ let timeLeft = x
+
+  return function() {
+    if (timeLeft < 0) {
+      console.log("Rockets already gone, bub !")
+        } else if ( timeLeft === 0) {
+      console.log("Blast Off!")
+        } else {
+      console.log("T-minus " + timeLeft + "...")
+        }
+        timeLeft -= 1
+      }
+    };
+
+const countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
